9a5fc17d2b19f8fb1720fb4a1f673e4022f54291
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-08-25 18:37:27 -0300

    first push

---
+++ src/main/java/com/w3lsolucoes/dscommerceprinc/DscommercePrincApplication.java
@@ -0,0 +1,13 @@
+package com.w3lsolucoes.dscommerceprinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommercePrincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommercePrincApplication.class, args);
+	}
+
+}
