e25bbc248f904a257b411ccdac1d0e35e90b96f7
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-08-26 19:07:49 -0300

    feat -> ProductRepository

---
+++ src/main/java/com/w3lsolucoes/dscommerceprinc/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.w3lsolucoes.dscommerceprinc.repositories;
+
+import com.w3lsolucoes.dscommerceprinc.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
+
+}
