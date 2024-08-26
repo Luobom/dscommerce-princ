824cea35788b75c2f026d2ea41b04e5042c98a66
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-08-26 19:13:52 -0300

    feat -> ProductService

---
+++ src/main/java/com/w3lsolucoes/dscommerceprinc/services/ProductService.java
@@ -0,0 +1,82 @@
+package com.w3lsolucoes.dscommerceprinc.services;
+
+import com.w3lsolucoes.dscommerceprinc.entities.Product;
+import com.w3lsolucoes.dscommerceprinc.repositories.ProductRepository;
+import com.w3lsolucoes.dscommerceprinc.services.exceptions.DataBaseException;
+import com.w3lsolucoes.dscommerceprinc.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.w3lsolucoes.dscommerceprinc.dto.ProductDTO;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        //Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(id));
+        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> productPage = repository.findAll(pageable);
+        return productPage.map(ProductDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findByName(String name, Pageable pageable) {
+        Page<Product> productPage = repository.searchByName(name, pageable);
+        return productPage.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO save(ProductDTO dto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(dto, product);
+        return new ProductDTO(repository.save(product));
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        try {
+            Product product = repository.getReferenceById(id);
+            String[] ignoredProperties = {"id"};
+            BeanUtils.copyProperties(dto, product, ignoredProperties);
+            return new ProductDTO(repository.save(product));
+        }catch (EntityNotFoundException | FatalBeanException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    @Transactional /*(propagation = Propagation.SUPPORTS)*/
+    public void delete(Long id) {
+        repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(id));
+        try {
+            repository.deleteById(id);
+            repository.flush();
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation");
+        }
+    }
+
+}
