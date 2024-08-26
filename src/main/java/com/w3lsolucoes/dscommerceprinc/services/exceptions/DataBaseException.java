8735948d6d5ada8b6bdc85f93b30dce10a006efb
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-08-26 19:10:46 -0300

    feat -> Exceptions

---
+++ src/main/java/com/w3lsolucoes/dscommerceprinc/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.w3lsolucoes.dscommerceprinc.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
