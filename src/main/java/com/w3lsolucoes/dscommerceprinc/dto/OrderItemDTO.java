0d4e0b4e188e31d7eae77640a01a3d5a85def467
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-09-29 18:54:42 -0300

    fix -> include imgUrl on OrderItemODTO

--- src/main/java/com/w3lsolucoes/dscommerceprinc/dto/OrderItemDTO.java
+++ src/main/java/com/w3lsolucoes/dscommerceprinc/dto/OrderItemDTO.java
@@ -6,17 +6,18 @@ public record OrderItemDTO(
         Long productId,
         String name,
         Double price,
+        String imgUrl,
         Integer quantity,
         Double subtotal
 
 ) {
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
-        this(productId, name, price, quantity, price * quantity);
+    public OrderItemDTO(Long productId, String name, Double price, String imgUrl, Integer quantity) {
+        this(productId, name, price, imgUrl, quantity, price * quantity );
     }
 
     public OrderItemDTO(OrderItem entity) {
-        this(entity.getProduct().getId(), entity.getProduct().getName(), entity.getPrice(), entity.getQuantity());
+        this(entity.getProduct().getId(), entity.getProduct().getName(), entity.getPrice(), entity.getProduct().getImgUrl(), entity.getQuantity());
     }
 
 }
